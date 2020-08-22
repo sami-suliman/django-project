9dd0aa5158d30dc6c651134f2f641be965685ad5
sami-suliman <sami.bco@gmail.com>
2020-08-22 14:32:04 -0500

    Include  hello app path into urls  project

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+#from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+ #   path("", views.index, name="index")
+    url("", views.index, name="index")
 ]
