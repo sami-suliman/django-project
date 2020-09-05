a930089a49076fbbd55727f318e1dff1c1eaa19a
sami-suliman <sami.bco@gmail.com>
2020-09-05 07:19:23 -0500

    Import forms from django

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,6 +1,11 @@
+from django import forms
 from django.shortcuts import render
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task:")
+    comlete = forms.BooleanField(label="Complete:")
 
 # Create your views here.
 def index(request):
@@ -9,5 +14,7 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
 
