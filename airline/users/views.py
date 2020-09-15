4099603b09b5437b53e05f70f91e27dffc692e37
sami-suliman <sami.bco@gmail.com>
2020-09-14 20:32:22 -0500

    Define index function inside users/view.py

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
